6b58a4fb549518284b45e7731ef7e2d0ccf7b4d1
SeeYa <89501533+Lixeer@users.noreply.github.com>
2024-08-18 00:21:43 +0800

    上点花样

--- src/main.py
+++ src/main.py
@@ -1,15 +1,36 @@
 import asyncio
-import websockets
-import json
-
-
 
 from libs.app import App
 from libs.app import config
 
 
 # 启动异步事件循环
+note="""
+ ▄▀▀█▄▄   ▄▀▀▄    ▄▀▀▄  ▄▀▄▄▄▄  
+▐ ▄▀   █ █   █    ▐  █ █ █    ▌ 
+  █▄▄▄▀  ▐  █        █ ▐ █      
+  █   █    █   ▄    █    █      
+ ▄▀▄▄▄▀     ▀▄▀ ▀▄ ▄▀   ▄▀▄▄▄▄▀ 
+█    ▐            ▀    █     ▐  
+▐           
+"""
 
-app=App(address=config.ADDRESS, port=config.PORT)
-if __name__=="__main__":
+b="""
+              ______     __     __     ______    
+ /\_/\       /\  == \   /\ \  _ \ \   /\  ___\   
+( o.o )      \ \  __<   \ \ \/ ".\ \  \ \ \____  
+ > ^ <        \ \_____\  \ \__/".~\_\  \ \_____\ 
+/     \        \/_____/   \/_/   \/_/   \/_____/ 
+                                    
+"""
+
+
+
+
+
+
+app = App(address=config.ADDRESS, port=config.PORT)
+if __name__ == "__main__":
+
+    print(b)
     asyncio.run(app.run())
