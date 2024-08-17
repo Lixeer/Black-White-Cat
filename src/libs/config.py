e19520e9c35da910b3092b03ea6dedfbad4f2cf9
SeeYa <89501533+Lixeer@users.noreply.github.com>
2024-08-17 21:20:48 +0800

    增加颜色输出并修复在控制台中输出不兼容的问题

--- src/libs/config.py
+++ src/libs/config.py
@@ -5,4 +5,4 @@ VALID_KEY="caixukun66"
 PORT=2048
 ADDRESS="0.0.0.0"
 
-LOG_LEVEL = 3   #0.None 1.info 2.warn 3.error
+LOG_LEVEL = 4  #0.None 1.info 2.warn 3.error
--- src/libs/loger/__init__.py
+++ src/libs/loger/__init__.py
@@ -1,35 +1,45 @@
 import inspect
 import datetime
 import enum
+import os
 
 import libs.config
 
 from colorama import Fore
-
+os.system("")
 class Level(enum.Enum):
     NONE=0
-    INFO=1
-    WARN=2
-    ERROR=3
+    DEBUG=1
+    INFO=2
+    WARN=3
+    ERROR=4
 
 class Loger:
     def __init__(self,level):
         self.level=level
 
+
+
     def info(self,content:str):
         if self.level>=Level.INFO.value:
             filename,line,time=self._get_call_stack()
-            print(f"{Fore.GREEN}{time}[{Level.INFO.name}] - {content} | {filename} line:{line} ")
+            print(f"[\033[40;36m{time}\033[0m] [\033[40;32mINFO\033[0m] \033[40;36;4m{filename}:{line}\033[0m | \033[40;32m{content}\033[0m")
+
 
     def warn(self,content:str):
         if self.level>=Level.WARN.value:
             filename,line,time=self._get_call_stack()
-            print(f"{Fore.YELLOW}{time}[{Level.WARN.name}] - {content} | {filename} line:{line} ")
+            print(f"[\033[40;36m{time}\033[0m] [\033[40;33mWARNING\033[0m] \033[40;36;4m{filename}:{line}\033[0m | \033[40;33m{content}\033[0m")
 
     def error(self,content:str):
         if self.level>=Level.ERROR.value:
             filename,line,time=self._get_call_stack()
-            print(f"{Fore.RED}{time}[{Level.ERROR.name}] - {content} | {filename} line:{line} ")
+            print(f"[\033[40;36m{time}\033[0m] [\033[40;31mERROR\033[0m] \033[40;36;4m{filename}:{line}\033[0m | \033[40;31m{content}\033[0m")
+
+    def debug(self,content:str):
+        if self.level>=Level.DEBUG.value:
+            filename,line,time=self._get_call_stack()
+            print(f"[\033[40;36m{time}\033[0m] [\033[40;37mDEBUG\033[0m] \033[40;36;4m{filename}:{line}\033[0m | \033[40;37m{content}\033[0m")
 
     def _get_call_stack(self):
         caller_frame = inspect.stack()[2]
